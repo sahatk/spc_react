2973aaf13b9b9e96c74732efb8f95bc259e7a3f7
sangjin Park <sahatk@naver.com>
2022-08-05 20:04:57 +0900

    2022-08-05

--- src/Pages/Media_Hub/Cf/Cf_box1.js
+++ src/Pages/Media_Hub/Cf/Cf_box1.js
@@ -7,10 +7,10 @@ import './Cf_box1.css';
 function Cf_box1() {
   return (
     <section id="Cf_box1">
-        <h1 class="blind">SPC NEW CF</h1>
-        <div class="container">
-            <div class="cf_title"><p class="font_50">new cf</p></div>
-            <div class="cf_video">
+        <h1 className="blind">SPC NEW CF</h1>
+        <div className="container">
+            <div className="cf_title"><p className="font_50">new cf</p></div>
+            <div className="cf_video">
                 <iframe width="1287" height="724" src="https://www.youtube.com/embed/U6mKE_DF1QE?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
