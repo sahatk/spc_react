49ca42adbb1d9baf62fccf3174a5893f4c531dbf
sangjin Park <sahatk@naver.com>
2022-08-05 20:14:40 +0900

    2022-08-05

--- src/Pages/Business/Subsidiary/Subsidiary_box1.js
+++ src/Pages/Business/Subsidiary/Subsidiary_box1.js
@@ -31,7 +31,7 @@ function Subsidiary_box1() {
                     </li>
                     <li className="subsidiary_card_li">
                         <div className="food_card_image_02">
-                            <h2 className="blind">식품 이미지_01</h2>
+                            <h2 className="blind">식품 이미지_02</h2>
                         </div>
                         <div className="subsidiary_card_text_wrap">
                             <div className="subsidiary_card_text_01">
@@ -49,14 +49,14 @@ function Subsidiary_box1() {
                 <ul className="subsidiary_card_ul">
                     <li className="subsidiary_card_li">
                         <div className="food_card_image_03">
-                            <h2 className="blind">식품 이미지_01</h2>
+                            <h2 className="blind">식품 이미지_03</h2>
                         </div>
                         <div className="subsidiary_card_text_wrap">
                             <div className="subsidiary_card_text_01">
                                 <a href="#" className="font_20">비알코리아</a>
                             </div>
                             <div className="subsidiary_card_text_02">
-                                <p className="font_18">행복한 세상을 만드는 최고 수준의 식품생명공학기업</p>
+                                <p className="font_18">We Make People Happy!</p>
                             </div>
                             <div className="view_more">
                                 <a target="_blank" href="https://www.naver.com/">more view</a>
@@ -75,14 +75,14 @@ function Subsidiary_box1() {
                 <ul className="subsidiary_card_ul">
                     <li className="subsidiary_card_li">
                         <div className="material_card_image_01">
-                            <h2 className="blind">식품 이미지_01</h2>
+                            <h2 className="blind">원료 이미지_01</h2>
                         </div>
                         <div className="subsidiary_card_text_wrap">
                             <div className="subsidiary_card_text_01">
-                                <a href="#" className="font_20">spc 삽립</a>
+                                <a href="#" className="font_20">spl</a>
                             </div>
                             <div className="subsidiary_card_text_02">
-                                <p className="font_18">행복한 세상을 만드는 최고 수준의 식품생명공학기업</p>
+                                <p className="font_18">아시아 최대 규모의 휴면 반죽메이커</p>
                             </div>
                             <div className="view_more">
                                 <a target="_blank" href="https://www.naver.com/">more view</a>
@@ -91,14 +91,14 @@ function Subsidiary_box1() {
                     </li>
                     <li className="subsidiary_card_li">
                         <div className="material_card_image_02">
-                            <h2 className="blind">식품 이미지_01</h2>
+                            <h2 className="blind">원료 이미지_02</h2>
                         </div>
                         <div className="subsidiary_card_text_wrap">
                             <div className="subsidiary_card_text_01">
-                                <a href="#" className="font_20">파리크라상</a>
+                                <a href="#" className="font_20">spc pack</a>
                             </div>
                             <div className="subsidiary_card_text_02">
-                                <p className="font_18">프랑스 전통의 베이커리 프랜차이즈</p>
+                                <p className="font_18">친환경 PACKAGING</p>
                             </div>
                             <div className="view_more">
                                 <a target="_blank" href="https://www.naver.com/">more view</a>
@@ -112,19 +112,19 @@ function Subsidiary_box1() {
             {/* <!-- 계열사 IT/서비스 박스--> */}
             <div className="subsidiary_card_box_half">
                 <div className="subsidiary_title">
-                    <div className="subsidiary_title_logo font_20">원료</div>
+                    <div className="subsidiary_title_logo font_20">IT/서비스</div>
                 </div>
                 <ul className="subsidiary_card_ul">
                     <li className="subsidiary_card_li">
                         <div className="service_card_image_01">
-                            <h2 className="blind">식품 이미지_01</h2>
+                            <h2 className="blind">IT/서비스 이미지_01</h2>
                         </div>
                         <div className="subsidiary_card_text_wrap">
                             <div className="subsidiary_card_text_01">
-                                <a href="#" className="font_20">spc 삽립</a>
+                                <a href="#" className="font_20">(주)섹티나인</a>
                             </div>
                             <div className="subsidiary_card_text_02">
-                                <p className="font_18">행복한 세상을 만드는 최고 수준의 식품생명공학기업</p>
+                                <p className="font_18">Total IT Service 및 마케팅 전문기업</p>
                             </div>
                             <div className="view_more">
                                 <a target="_blank" href="https://www.naver.com/">more view</a>
@@ -138,19 +138,19 @@ function Subsidiary_box1() {
             {/* <!-- 계열사 유통 박스--> */}
             <div className="subsidiary_card_box_half">
                 <div className="subsidiary_title">
-                    <div className="subsidiary_title_logo font_20">원료</div>
+                    <div className="subsidiary_title_logo font_20">유통</div>
                 </div>
                 <ul className="subsidiary_card_ul">
                     <li className="subsidiary_card_li">
                         <div className="distribution_card_image_01">
-                            <h2 className="blind">식품 이미지_01</h2>
+                            <h2 className="blind">유통 이미지_01</h2>
                         </div>
                         <div className="subsidiary_card_text_wrap">
                             <div className="subsidiary_card_text_01">
-                                <a href="#" className="font_20">spc 삽립</a>
+                                <a href="#" className="font_20">spc gfs</a>
                             </div>
                             <div className="subsidiary_card_text_02">
-                                <p className="font_18">행복한 세상을 만드는 최고 수준의 식품생명공학기업</p>
+                                <p className="font_18">글로벌 식품 식자재 유통 전문 기업</p>
                             </div>
                             <div className="view_more">
                                 <a target="_blank" href="https://www.naver.com/">more view</a>
@@ -159,14 +159,14 @@ function Subsidiary_box1() {
                     </li>
                     <li className="subsidiary_card_li">
                         <div className="distribution_card_image_02">
-                            <h2 className="blind">식품 이미지_01</h2>
+                            <h2 className="blind">유통 이미지_02</h2>
                         </div>
                         <div className="subsidiary_card_text_wrap">
                             <div className="subsidiary_card_text_01">
-                                <a href="#" className="font_20">파리크라상</a>
+                                <a href="#" className="font_20">타이거 인터내셔날</a>
                             </div>
                             <div className="subsidiary_card_text_02">
-                                <p className="font_18">프랑스 전통의 베이커리 프랜차이즈</p>
+                                <p className="font_18">땅과 인간에 대한 존중, 프랑스 와인 전문 수입사</p>
                             </div>
                             <div className="view_more">
                                 <a target="_blank" href="https://www.naver.com/">more view</a>
