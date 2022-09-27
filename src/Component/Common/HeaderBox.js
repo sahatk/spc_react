f30c67722bf80ccafafcf7690e936c4174393e92
sangjin Park <sahatk@naver.com>
2022-09-27 15:52:49 +0900

    2022-09-27

--- src/Component/Common/HeaderBox.js
+++ src/Component/Common/HeaderBox.js
@@ -38,12 +38,6 @@ function HeaderBox() {
       { id: uuid(), link: 'Media_Hub/CF', text: 'CF' }
     ],
     [
-      { id: uuid(), link: '#', text: 'SPC행복한재단' },
-      { id: uuid(), link: '#', text: '사회공헌 소개' },
-      { id: uuid(), link: '#', text: '사회공헌 소식' },
-      { id: uuid(), link: '#', text: '정도경영' }
-    ],
-    [
       { id: uuid(), link: '#', text: '인재채용' },
       { id: uuid(), link: '#', text: '인재양성' }
     ],
@@ -53,30 +47,93 @@ function HeaderBox() {
     ]
   ];
 
-  const prevScrollpos = window.pageYOffset;
-  window.onscroll = headerbarToggle;
-  function headerbarToggle() {
-    let style1 = `color:black;`;
-    let style2 = 'color:white;';
-    let global_white = `background-image:url('img/main_img/header_box/global_icon.png')`;
-    let global_black = `background-image:url('img/main_img/header_box/global_icon_black.png')`;
-    const headerBox = document.querySelector("#header_box");
-    const headerNavigation = headerBox.querySelector(".header_nav");
-    const headerUl = headerNavigation.querySelector(".main_menu");
-    const headerLi = headerUl.querySelectorAll(".main_li");
-    const currentScrollpos = window.pageYOffset;
-    if (prevScrollpos < currentScrollpos) {
-      headerBox.style.backgroundColor = 'white';
-      headerLi.forEach(function (el) {
-        el.style = style1;
-      });
-      headerLi[6].style = global_black;
-    } else {
-      headerBox.style.backgroundColor = 'transparent';
-      headerLi.forEach(el => {
-        el.style = style1;
-      });
-      headerLi[6].style = global_black;
+  // const prevScrollpos = window.pageYOffset;
+  // window.onscroll = headerbarToggle;
+  // function headerbarToggle() {
+  //   let style1 = `color:black;`;
+  //   let style2 = 'color:white;';
+  //   let global_white = `background-image:url('img/main_img/header_box/global_icon.png')`;
+  //   let global_black = `background-image:url('img/main_img/header_box/global_icon_black.png')`;
+  //   const headerBox = document.querySelector("#header_box");
+  //   const headerNavigation = headerBox.querySelector(".header_nav");
+  //   const headerUl = headerNavigation.querySelector(".main_menu");
+  //   const headerLi = headerUl.querySelectorAll(".main_li");
+  //   const currentScrollpos = window.pageYOffset;
+  //   if (prevScrollpos < currentScrollpos) {
+  //     headerBox.style.backgroundColor = 'white';
+  //     headerLi.forEach(function (el) {
+  //       el.style = style1;
+  //     });
+  //     headerLi[6].style = global_black;
+  //   } else {
+  //     headerBox.style.backgroundColor = 'transparent';
+  //     headerLi.forEach(el => {
+  //       el.style = style2;
+  //     });
+  //     headerLi[6].style = global_white;
+  //   }
+  // }
+
+  {
+    const prevScrollpos = window.pageYOffset;
+    window.onscroll = headerbarToggle;
+    function headerbarToggle() {
+      let style1 = `color:black;`;
+      let style2 = 'color:white;';
+      let global_white = `background-image:url('img/main_img/header_box/global_icon_white.png')`;
+      let global_black = `background-image:url('img/main_img/header_box/global_icon_black.png')`;
+      let logo_white = `background-image:url('img/main_img/header_box/gnb_logo_white.png')`;
+      let logo_gray = `background-image:url('img/main_img/header_box/gnb_logo_gray.png')`;
+      const headerBox = document.querySelector("#header_box");
+      const logo = document.querySelector(".logo_img");
+      const headerNavigation = headerBox.querySelector(".header_nav");
+      const headerUl = headerNavigation.querySelector(".main_menu");
+      const headerLi = headerUl.querySelectorAll(".main_li");
+      const currentScrollpos = window.pageYOffset;
+      if (prevScrollpos < currentScrollpos) {
+        headerBox.style.backgroundColor = 'white';
+        headerLi.forEach(function (el) {
+          el.style = style1;
+        });
+        headerLi[5].style = global_black;
+        logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
+    
+        headerBox.addEventListener('mouseover', function () {
+          headerLi[5].style = global_black;
+        });
+    
+      } else {
+        headerBox.style.backgroundColor = 'transparent';
+        headerLi.forEach(el => {
+          el.style = style2;
+        });
+        headerLi[5].style = global_white;
+        logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_white.png')`;
+      }
+    
+        /*스크롤 0 일떄 headerbox 오버시 로고 이미지 변경 */
+      if (currentScrollpos == 0) {
+        headerBox.addEventListener('mouseover', function () {
+          logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
+          headerLi[5].style = global_black;
+        });
+    
+        headerBox.addEventListener('mouseleave', function(){
+          logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_white.png')`;
+          headerLi[5].style = global_white;
+        });
+      } else {
+        headerBox.addEventListener('mouseover', function () {
+          logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
+          headerLi[5].style = global_black;
+        });
+    
+        headerBox.addEventListener('mouseleave', function(){
+          logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
+          headerLi[5].style = global_black;
+        });
+    
+      }
     }
   }
 
@@ -180,25 +237,6 @@ function HeaderBox() {
                     <ul className='sub_menu sub_menu_01'>
                       {
                         subMenuList[4].map(
-                          list =>
-                          <li key={list.id}>
-                            <NavLink to={list.link}>
-                              {list.text}
-                            </NavLink>
-                          </li>
-                        )
-                      }
-                    </ul>
-                  </div>
-              </li>
-              <li className='main_li'>
-                  <NavLink to={menuList[5].link}>
-                    {menuList[5].text}
-                  </NavLink>
-                  <div className='sub_menu_box'>
-                    <ul className='sub_menu sub_menu_01'>
-                      {
-                        subMenuList[5].map(
                           list =>
                           <li key={list.id}>
                             <NavLink to={list.link}>
